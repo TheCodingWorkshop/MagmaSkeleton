0fe17db54507c6fb566157bd6c797a13dbbc4de0
TheCodingWorkshop <riccodingworkshop@gmail.com>
2022-01-17 22:08:43 +0000

    added search controller

---
+++ App/Controller/Admin/SearchController.php
@@ -0,0 +1,53 @@
+<?php
+/*
+ * This file is part of the MagmaCore package.
+ *
+ * (c) Ricardo Miller <ricardomiller@lava-studio.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use MagmaCore\Base\Domain\Actions\SearchAction;
+use App\Entity\SearchEntity;
+use App\Forms\Admin\Search\SearchForm;
+use App\Event\SearchActionEvent;
+
+class SearchController extends \MagmaCore\Administrator\Controller\AdminController
+{
+
+    public function __construct(array $routeParams)
+    {
+        parent::__construct($routeParams);
+
+        $this->addDefinitions(
+            [
+                'searchAction' => SearchAction::class,
+                'searchForm' => SearchForm::class
+            ]
+        );
+    }
+
+    protected function indexAction()
+    {
+
+    }
+
+    protected function searchAction()
+    {
+        $this->searchAction
+            ->execute($this, SearchEntity::class, SearchActionEvent::class, NULL, __METHOD__)
+            ->render()
+            ->with()
+            ->form($this->searchForm)
+            ->end();
+    }
+
+
+}
+
+
