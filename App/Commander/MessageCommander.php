c5c08219abfc19535798af2ed432640659421cd0
TheCodingWorkshop <riccodingworkshop@gmail.com>
2022-01-17 12:59:50 +0000

    added message controller

---
+++ App/Commander/MessageCommander.php
@@ -0,0 +1,92 @@
+<?php
+/*
+ * This file is part of the MagmaCore package.
+ *
+ * (c) Ricardo Miller <ricardomiller@lava-studio.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Commander;
+
+use App\Model\MessageModel;
+use Exception;
+use MagmaCore\CommanderBar\ApplicationCommanderInterface;
+use MagmaCore\CommanderBar\ApplicationCommanderTrait;
+use MagmaCore\CommanderBar\CommanderUnsetterTrait;
+
+class MessageCommander extends MessageModel implements ApplicationCommanderInterface
+{
+
+    use ApplicationCommanderTrait;
+    use CommanderUnsetterTrait;
+
+    /**
+     * Return an array of all the inner routes within the user model
+     * @var array
+     */
+    protected const INNER_ROUTES = [
+        'index',
+        'new',
+        'show',
+        'reply',
+        'edit',
+        'assigned'
+    ];
+
+    private array $noCommander = [];
+    private array $noNotification = self::INNER_ROUTES;
+    private array $noCustomizer = ['edit', 'show'];
+    private array $noManager = ['show'];
+    private array $noAction = [];
+    private array $noFilter = ['edit', 'show'];
+
+    private object $controller;
+
+    /**
+     * Get the specific yaml file which helps to render some text within the specified
+     * html template.
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function getYml(): array
+    {
+        return $this->findYml('message');
+    }
+
+    /**
+     * Display a sparkline graph for this controller index route
+     *
+     * @return string
+     */
+    public function getGraphs(): string
+    {
+        return '<div id="sparkline"></div>';
+    }
+
+    /**
+     * Dynamically change commander bar header based on the current route
+     *
+     * @param object $controller
+     * @return string
+     * @throws Exception
+     */
+    public function getHeaderBuild(object $controller): string
+    {
+        $this->getHeaderBuildException($controller, self::INNER_ROUTES);
+
+        $this->controller = $controller;
+        return match ($controller->thisRouteAction()) {
+            'index' => $this->getStatusColumnFromQueryParams($controller),
+            'new' => 'Message',
+            'show' => 'View Message',
+            'reply' => 'Reply',
+            'edit' => "Edit " . $this->getHeaderBuildEdit($controller, 'name'),
+            default => "Unknown"
+        };
+    }
+}
---
+++ App/Controller/Admin/MessageController.php
@@ -0,0 +1,158 @@
+<?php
+/*
+ * This file is part of the MagmaCore package.
+ *
+ * (c) Ricardo Miller <ricardomiller@lava-studio.co.uk>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use MagmaCore\Base\Access;
+use App\Model\MessageModel;
+use App\Entity\MessageEntity;
+use App\Schema\MessageSchema;
+use App\Event\MessageActionEvent;
+use App\DataColumns\MessageColumn;
+use App\Commander\MessageCommander;
+use MagmaCore\UserManager\UserModel;
+use App\Forms\Admin\Message\MessageForm;
+use App\Forms\Admin\Message\MessageReplyForm;
+use MagmaCore\Base\Domain\Actions\ReplyAction;
+
+class MessageController extends \MagmaCore\Administrator\Controller\AdminController
+{ 
+
+
+    public function __construct(array $routeParams)
+    {
+        parent::__construct($routeParams);
+
+        $this->addDefinitions(
+            [
+                'repository' => MessageModel::class,
+                'column' => MessageColumn::class,
+                'commander' => MessageCommander::class,
+                'entity' => MessageEntity::class,
+                'messageForm' => MessageForm::class,
+                'messageReplyForm' => MessageReplyForm::class,
+                'userModel' => UserModel::class,
+                'replyAction' => ReplyAction::class
+            ]
+        );
+    }
+
+    /**
+     * Returns a 404 error page if the data is not present within the database
+     * else return the requested object
+     *
+     * @return mixed
+     */
+    public function findOr404(): mixed
+    {
+        if (isset($this)) {
+            return $this->repository->getRepo()
+                ->findAndReturn($this->thisRouteID())
+                ->or404();
+        }
+    }
+
+    protected function indexAction()
+    {
+        $this->indexAction
+            ?->setAccess($this, Access::CAN_VIEW)
+            ?->execute($this, NULL, NULL, MessageSchema::class, __METHOD__)
+            ?->render()
+            ?->with()
+            ?->table()
+            ?->end();
+    }
+
+    protected function newAction()
+    {
+        $this->newAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__)
+            ->render()
+            ->with()
+            ->form($this->messageForm)
+            ->end();
+    }
+
+    protected function showAction()
+    {
+        $this->showAction
+            ->execute($this, NULL, NULL, NULL, __METHOD__)
+            ->render()
+            ->with(
+                [
+                    'user' => $this->toArray($this->userModel->getUser((int)$_GET['user'])),
+                    'row' => $this->toArray($this->findOr404())
+                ]
+            )
+            ->form($this->messageForm)
+            ->end();
+    }
+
+    protected function replyAction()
+    {
+        $this->replyAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__)
+            ->render()
+            ->with()
+            ->form($this->messageReplyForm)
+            ->end();
+    }
+
+    protected function forwardAction()
+    {
+
+    }
+
+    /**
+     * Change a user status to lock
+     */
+    protected function starredAction()
+    {
+        $this->changeStatusAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__, [], [], ['is_starred' => 1])
+            ->endAfterExecution();
+    }
+
+    /**
+     * Change a user status to lock
+     */
+    protected function unstarredAction()
+    {
+        $this->changeStatusAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__, [], [], ['is_starred' => 0])
+            ->endAfterExecution();
+    }
+
+        /**
+     * Change a user status to lock
+     */
+    protected function markedAction()
+    {
+        $this->changeStatusAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__, [], [], ['is_marked' => 1])
+            ->endAfterExecution();
+    }
+
+    /**
+     * Change a user status to lock
+     */
+    protected function unmarkedAction()
+    {
+        $this->changeStatusAction
+            ->execute($this, MessageEntity::class, MessageActionEvent::class, NULL, __METHOD__, [], [], ['is_marked' => 0])
+            ->endAfterExecution();
+    }
+
+
+
+}
+
