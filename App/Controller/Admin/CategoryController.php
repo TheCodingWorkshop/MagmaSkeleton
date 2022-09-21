3c8c9b376c801b51e1aa10b561022a6596f72b0d
TheCodingWorkshop <riccodingworkshop@gmail.com>
2022-09-21 20:47:03 +0100

    September update

--- App/Controller/Admin/CategoryController.php
+++ App/Controller/Admin/CategoryController.php
@@ -61,7 +61,8 @@ class CategoryController extends \MagmaCore\Administrator\Controller\AdminContro
     {
         $this->newAction
             ?->setAccess($this, Access::CAN_VIEW)
-            ->execute($this, CategoryEntity::class, CategoryActionEvent::class, NULL, __METHOD__)
+            ?->execute(controller:$this, entity:CategoryEntity::class, event:CategoryActionEvent::class, method:__METHOD__)
+            //->execute($this, CategoryEntity::class, CategoryActionEvent::class, NULL, __METHOD__)
             ->render()
             ->with()
             ->form($this->categoryForm)
